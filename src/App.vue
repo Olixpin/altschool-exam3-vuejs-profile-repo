456a4605145fc4fa5236e7249c17e17882f704a2
Olixpin <noibiabdulrasaq@gmail.com>
2023-03-09 23:21:37 +0100

    .

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="bg-gray-100 h-full">
     <header class="bg-white shadow h-16">
-      <router-link to="/">
+      <router-link :to="{ name: 'home' }">
         <!-- <img
           class="h-16 w-16 float-left"
           src="https://www.vuemastery.com/images/challenges/vuemasterylogo.svg"
